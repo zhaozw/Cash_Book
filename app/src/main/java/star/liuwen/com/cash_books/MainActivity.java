2e736d5313f45fcf2c2bf042842aa010d68414a0
liuwen <370494581@qq.com>
2016-12-28 12:24:48 +0800

    初始化项目 v1.0.0

---
+++ app/src/main/java/star/liuwen/com/cash_books/MainActivity.java
@@ -0,0 +1,13 @@
+package star.liuwen.com.cash_books;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
